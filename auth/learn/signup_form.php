70d78982f9335ec28a303594fe5e3e5e3408f904
Paolo <paolo@rcvsknowledge.org>
2023-08-22 09:59:37 +0000

    RCVSK Learn auth: Remove security question message on failed login

--- auth/learn/signup_form.php
+++ auth/learn/signup_form.php
@@ -79,7 +79,6 @@ class login_signup_form extends moodleform implements renderable, templatable {
         profile_signup_fields($mform);
 
         if (signup_captcha_enabled()) {
-            $mform->addElement('recaptcha', 'recaptcha_element', get_string('security_question', 'auth'));
             $mform->addHelpButton('recaptcha_element', 'recaptcha', 'auth');
             $mform->closeHeaderBefore('recaptcha_element');
         }
